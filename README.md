d5137535cad8e2d8e2c42416efe54b3748aebcf4
cassicakes <aguiar.cas@gmail.com>
2016-02-02 14:49:36 -0800

    before i mess with flip

--- README.md
+++ README.md
@@ -3,4 +3,23 @@
 ![Mock Up Screen Shot]
 (Mock-Screen shot.png)
 
+Above is my wireframe.
+
+The top left is a picture, with the title flowing horizontally. (allows more room for the board)
+	--Hoping to place a funny Nic Cage head image, overlapping the title "Memory"
+
+Two buttons at the top floating over another image. One button being a reset and another being a click tracker. 
+	--The click tracker is a way to make the game two player. You can try to win the board by beathing your competitor's score. 
+	--Reset will turn all the cards back over, and "shuffle" the images in the array so the next game is different.
+
+Next is the game board with 25 "cards" 
+	--Click the card to turn over to see the image, and click another card to find it's match. If it is a match the cards will stay turned over, if no match they will automatically turn back over. Continue till all of board is matched.
+
+That's it!
+
+
+
+
+
+
 
--- js/script.js
+++ js/script.js
@@ -1,21 +1,69 @@
 var allCards = $("#boxcard div");
 var allImages = [ "images/bulbasaur.jpg" , "images/caterpee.jpg" , "images/charmander.jpg" , "images/eevee.jpg" , "images/jigglypuff.jpg" , "images/meowth.jpg" , "images/pikachu.jpg" , "images/psyduck.jpg" , "images/squirttle.jpg" , "images/weedle.jpg" , "images/bulbasaur.jpg" , "images/caterpee.jpg" , "images/charmander.jpg" , "images/eevee.jpg" , "images/jigglypuff.jpg" , "images/meowth.jpg" , "images/pikachu.jpg" , "images/psyduck.jpg" , "images/squirttle.jpg" , "images/weedle.jpg"];
 
-var randomIndex = '';
+var nthChildCounter = 1;
+var clickCounter = 0;
+var clickedImage1 = 0;
+var clickedImage2 = 0;
+
+
+//used the Fisher-Yates Shuffle:
+
+var shuffle = function(array) {
+	"use strict";
+    let counter = array.length;
+
+    // While there are elements in the array
+    while (counter > 0) {
+        // Pick a random index
+        let index = Math.floor(Math.random() * counter);
+
+        // Decrease counter by 1
+        counter--;
+
+        // And swap the last element with it
+        let temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+    }
+
+    return array;
+}
 
 $(document).ready(function() {
-	
-	for (i = 1; i <= allCards.length; i++) {
-	randomIndex = Math.floor(Math.random() * allCards.length)
-	console.log(randomIndex)
- 	// This prints a random number set from 0-19 but repeats it's self. 
-	$("#boxcard div:nth-child(" + i + ")").css("background-image" , "url(" + allImages[randomIndex] + ")");
- 	// 
-};
-	
-	// $("#boxcard div:nth-child(4)").css("background-image" , "url(" + allImages[3] + ")");
 
+	// shuffle the deck
+	var newDeck = shuffle(allImages);
+	
+	//assign iamges to cards randomly
+	for (i = 0; i <= allCards.length; i++) {
+ 		// This variable is so that counter will go from 1 to 20 cause nth child starts at 1, an array starts at 0, vaiable declared above so outside of loop and isn't reestablished at 1.
 
-//end loop
+		$("#boxcard div:nth-child(" + nthChildCounter + ")").css("background-image" , "url(" + newDeck[i] + ")");
+
+		nthChildCounter++;
+	};
+
+	// player clicks div
+	// computer checks div and stores img in var
+	// player clicks second div
+	// computer checks div and stores img in var (wether or not they are same)
+	$("#boxcard div").on("click", function() {
+		clickCounter++; 
+		if (clickCounter % 2 !== 0 ) {
+			clickedImage1 = $(this).css("background-image");
+			console.log(clickedImage1);
+		} else if (clickCounter % 2 == 0 ) {
+			clickedImage2 = $(this).css("background-image");
+			console.log(clickedImage2);
+			// need another if statement to check if image is the same 
+			if (clickedImage1 == clickedImage2 ) {
+				alert("A Match!");
+			} else {
+				alert("Try again");
+			}
+		};
+});
+
 });
 

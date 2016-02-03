ffca747a535b17f5bae66ea14bc50eec6ff26eaa
cassicakes <aguiar.cas@gmail.com>
2016-02-02 19:11:40 -0800

    with reset and counter

--- js/script.js
+++ js/script.js
@@ -7,7 +7,8 @@ var clickedImage1 = 0;
 var clickedImage2 = 0;
 var clickedCard1 =  0;
 var clickedCard2 = 0;
-
+var newDeck;
+var matchedCounter = 0;
 
 //used the Fisher-Yates Shuffle:
 
@@ -28,34 +29,36 @@ var shuffle = function(array) {
         array[counter] = array[index];
         array[index] = temp;
     }
-
     return array;
 }
 
 $(document).ready(function() {
 
-
 	$(".card").flip ({
 		axis: "y",
 		trigger: "click"
 	});
-	// shuffle the deck
-	var newDeck = shuffle(allImages);
-	
-	//assign iamges to cards randomly
-	for (i = 0; i < newDeck.length; i++) {
- 		// This variable is so that counter will go from 1 to 20 cause nth child starts at 1, an array starts at 0, vaiable declared above so outside of loop and isn't reestablished at 1.
-		$("div .card:nth-child(" + nthChildCounter + ") .back").css("background-image" , "url(" + newDeck[i] + ")");
 
-		nthChildCounter++;
+	function setBoard(){
+		nthChildCounter=1;
+		// shuffle the deck
+		newDeck = shuffle(allImages);
+		//assign iamges to cards randomly
+		for (i = 0; i < newDeck.length; i++) {
+	 		// This variable is so that counter will go from 1 to 20 cause nth child starts at 1, an array starts at 0, vaiable declared above so outside of loop and isn't reestablished at 1.
+	 		
+			$("div .card:nth-child(" + nthChildCounter + ") .back").css("background-image" , "url(" + newDeck[i] + ")");
+
+			nthChildCounter++;
+		};
 	};
-
 	// player clicks div
 	// computer checks div and stores img in var
 	// player clicks second div
 	// computer checks div and stores img in var (wether or not they are same)
 	$("#boxcard div .front").on("click", function() {
 		clickCounter++; 
+		$("#counter").html(clickCounter);
 		if (clickCounter % 2 !== 0 ) {
 			clickedCard1 = $(this);
 			clickedImage1 = $(this).siblings().css("background-image");
@@ -64,6 +67,13 @@ $(document).ready(function() {
 			clickedCard2 = $(this);
 			// need another if statement to check if image is the same 
 			if (clickedImage1 == clickedImage2 ) {
+				// clickedCard1.children().off('.flip');
+				// figure oout how to make pairs unclickable
+				// clickedCard2.children().off(".flip");
+				matchedCounter++;
+				if (matchedCounter == 10) {
+					setTimeout(alert("You win baby girl!"), 1000);
+				}
 			} else {
 				setTimeout(function(){
 					clickedCard1.parent().flip('toggle');
@@ -72,6 +82,15 @@ $(document).ready(function() {
 			}
 		};
 	});
+	$("#reset").on("click", function() {
+		clickCounter = 0;
+		setBoard();
+		$(".card").flip(false);
+		$("#counter").html(clickCounter);
+		matchedCounter = 0;
+	});
+
+setBoard();
 
 });
 
